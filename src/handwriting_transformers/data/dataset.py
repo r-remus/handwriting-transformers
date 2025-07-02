ccd1b2af732548bbd81f7956d038ed9b841319c6
Robert Remus <r.remus@paradatec.de>
2025-07-02 16:01:41 +0200

    Boyscout: Improve formatting and remove unused imports in dataset.py

--- src/handwriting_transformers/data/dataset.py
+++ src/handwriting_transformers/data/dataset.py
@@ -1,18 +1,12 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT
 
-import random
 import torch
 from torch.utils.data import Dataset
-from torch.utils.data import sampler
-#import lmdb
 import torchvision.transforms as transforms
-import six
-import sys
 from PIL import Image
 import numpy as np
 import os
-import sys
 import pickle
 import numpy as np
 from handwriting_transformers.params import *
@@ -28,7 +22,6 @@ def crop_(input):
     return np.array(cropped_image)
 
 def get_transform(grayscale=False, convert=True):
-
     transform_list = []
     if grayscale:
         transform_list.append(transforms.Grayscale(1))
@@ -43,44 +36,33 @@ def get_transform(grayscale=False, convert=True):
     return transforms.Compose(transform_list)
 
 def load_itw_samples(folder_path, num_samples = 15):
+    paths = glob.glob(f'{folder_path}/*')
+    paths = np.random.choice(paths, num_samples, replace = len(paths)<=num_samples)
+    words = [os.path.basename(path_i)[:-4] for path_i in paths]
+    imgs = [np.array(Image.open(i).convert('L')) for i in paths]
+    imgs = [crop_(im) for im in imgs]
+    imgs = [cv2.resize(imgs_i, (int(32*(imgs_i.shape[1]/imgs_i.shape[0])), 32)) for imgs_i in imgs]
+    max_width = 192
+    imgs_pad = []
+    imgs_wids = []
+    trans_fn = get_transform(grayscale=True)
+    for img in imgs:
+        img = 255 - img
+        img_height, img_width = img.shape[0], img.shape[1]
+        outImg = np.zeros(( img_height, max_width), dtype='float32')
+        outImg[:, :img_width] = img[:, :max_width]
+        img = 255 - outImg
+        imgs_pad.append(trans_fn((Image.fromarray(img))))
+        imgs_wids.append(img_width)
 
-  paths = glob.glob(f'{folder_path}/*')
-  paths = np.random.choice(paths, num_samples, replace = len(paths)<=num_samples)
+    imgs_pad = torch.cat(imgs_pad, 0)
 
-  words = [os.path.basename(path_i)[:-4] for path_i in paths]
-
-  imgs = [np.array(Image.open(i).convert('L')) for i in paths]
-
-  imgs =  [crop_(im) for im in imgs]
-  imgs = [cv2.resize(imgs_i, (int(32*(imgs_i.shape[1]/imgs_i.shape[0])), 32)) for imgs_i in imgs]
-  max_width = 192
-
-  imgs_pad = []
-  imgs_wids = []
-
-  trans_fn = get_transform(grayscale=True)
-
-  for img in imgs:
-
-      img = 255 - img
-      img_height, img_width = img.shape[0], img.shape[1]
-      outImg = np.zeros(( img_height, max_width), dtype='float32')
-      outImg[:, :img_width] = img[:, :max_width]
-
-      img = 255 - outImg
-
-      imgs_pad.append(trans_fn((Image.fromarray(img))))
-      imgs_wids.append(img_width)
-
-  imgs_pad = torch.cat(imgs_pad, 0)
-
-  return imgs_pad.unsqueeze(0), torch.Tensor(imgs_wids).unsqueeze(0)
+    return imgs_pad.unsqueeze(0), torch.Tensor(imgs_wids).unsqueeze(0)
 
 
-class TextDataset():
+class TextDataset:
 
     def __init__(self, base_path = DATASET_PATHS,  num_examples = 15, target_transform=None):
-
         self.NUM_EXAMPLES = num_examples
   
         #base_path = DATASET_PATHS
@@ -93,19 +75,13 @@ class TextDataset():
 
         self.transform = get_transform(grayscale=True)
         self.target_transform = target_transform
-        
         self.collate_fn = TextCollator()
-
 
     def __len__(self):
         return len(self.author_id)
 
     def __getitem__(self, index):
-
-        
-
         NUM_SAMPLES = self.NUM_EXAMPLES
-
 
         author_id = self.author_id[index]
 
@@ -117,41 +93,37 @@ class TextDataset():
         real_labels = self.IMG_DATA_AUTHOR[rand_id_real]['label'].encode()
 
         imgs = [np.array(self.IMG_DATA_AUTHOR[idx]['img'].convert('L')) for idx in random_idxs]
-        labels = [self.IMG_DATA_AUTHOR[idx]['label'].encode() for idx in random_idxs]
        
         max_width = 192 #[img.shape[1] for img in imgs] 
         
         imgs_pad = []
         imgs_wids = []
-
         for img in imgs:
-
             img = 255 - img
             img_height, img_width = img.shape[0], img.shape[1]
             outImg = np.zeros(( img_height, max_width), dtype='float32')
             outImg[:, :img_width] = img[:, :max_width]
-
             img = 255 - outImg
-
             imgs_pad.append(self.transform((Image.fromarray(img))))
             imgs_wids.append(img_width)
 
         imgs_pad = torch.cat(imgs_pad, 0)
         
-
-        item = {'simg': imgs_pad, 'swids':imgs_wids, 'img' : real_img, 'label':real_labels,'img_path':'img_path', 'idx':'indexes', 'wcl':index}
-    
-
-
+        item = {
+            'simg': imgs_pad,
+            'swids': imgs_wids,
+            'img': real_img,
+            'label': real_labels,
+            'img_path': 'img_path',
+            'idx': 'indexes',
+            'wcl': index,
+        }
         return item
 
 
-
-
-class TextDatasetval():
+class TextDatasetval:
 
     def __init__(self, base_path = DATASET_PATHS, num_examples = 15, target_transform=None):
-        
         self.NUM_EXAMPLES = num_examples
         #base_path = DATASET_PATHS
         file_to_store = open(base_path, "rb")
@@ -163,7 +135,6 @@ class TextDatasetval():
 
         self.transform = get_transform(grayscale=True)
         self.target_transform = target_transform
-        
         self.collate_fn = TextCollator()
     
 
@@ -171,7 +142,6 @@ class TextDatasetval():
         return len(self.author_id)
 
     def __getitem__(self, index):
-
         NUM_SAMPLES = self.NUM_EXAMPLES
 
         author_id = self.author_id[index]
@@ -183,9 +153,7 @@ class TextDatasetval():
         real_img = self.transform(self.IMG_DATA_AUTHOR[rand_id_real]['img'].convert('L'))
         real_labels = self.IMG_DATA_AUTHOR[rand_id_real]['label'].encode()
 
-
         imgs = [np.array(self.IMG_DATA_AUTHOR[idx]['img'].convert('L')) for idx in random_idxs]
-        labels = [self.IMG_DATA_AUTHOR[idx]['label'].encode() for idx in random_idxs]
        
         max_width = 192 #[img.shape[1] for img in imgs] 
         
@@ -193,30 +161,30 @@ class TextDatasetval():
         imgs_wids = []
 
         for img in imgs:
-
             img = 255 - img
             img_height, img_width = img.shape[0], img.shape[1]
             outImg = np.zeros(( img_height, max_width), dtype='float32')
             outImg[:, :img_width] = img[:, :max_width]
-
             img = 255 - outImg
-
             imgs_pad.append(self.transform((Image.fromarray(img))))
             imgs_wids.append(img_width)
 
         imgs_pad = torch.cat(imgs_pad, 0)
         
-
-        item = {'simg': imgs_pad, 'swids':imgs_wids, 'img' : real_img, 'label':real_labels,'img_path':'img_path', 'idx':'indexes', 'wcl':index}
-    
-
-
+        item = {
+            'simg': imgs_pad,
+            'swids': imgs_wids,
+            'img': real_img,
+            'label': real_labels,
+            'img_path': 'img_path',
+            'idx': 'indexes',
+            'wcl': index,
+        }
         return item
 
 
+class TextCollator:
 
-
-class TextCollator(object):
     def __init__(self):
         self.resolution = resolution
 
@@ -225,9 +193,9 @@ class TextCollator(object):
         img_path = [item['img_path'] for item in batch]
         width = [item['img'].shape[2] for item in batch]
         indexes = [item['idx'] for item in batch]
-        simgs =  torch.stack([item['simg'] for item in batch], 0)
-        wcls =  torch.Tensor([item['wcl'] for item in batch])
-        swids =  torch.Tensor([item['swids'] for item in batch])
+        simgs = torch.stack([item['simg'] for item in batch], 0)
+        wcls = torch.Tensor([item['wcl'] for item in batch])
+        swids = torch.Tensor([item['swids'] for item in batch])
         imgs = torch.ones([len(batch), batch[0]['img'].shape[0], batch[0]['img'].shape[1], max(width)], dtype=torch.float32)
         for idx, item in enumerate(batch):
             try:
@@ -242,4 +210,3 @@ class TextCollator(object):
             z = torch.stack([item['z'] for item in batch])
             item['z'] = z
         return item
-
